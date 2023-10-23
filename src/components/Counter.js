22381f0c0c1244d5e52b2514e62dea8d8323beab
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-23 14:49:32 +0900

    state 여러 개 사용
    - store 에서 여러 개의 state를 object 형태로 사용 가능

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,9 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter)
   const dispatch = useDispatch();
+
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'})
+  };
 
   const incrementHandler = () => {
     dispatch({type: 'increment'})
@@ -21,7 +25,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>토글 카운터</button>
       <button onClick={incrementHandler}>숫자 증가</button>
       <button onClick={increase10Handler}>숫자 10씩 증가</button>
