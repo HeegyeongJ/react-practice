a48efdc2a88004db9c410a53b18ced668fe51fa5
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-24 15:35:21 +0900

    Update Greeting.test.js
    - 오류 예시

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,12 +1,35 @@
 import { render, screen } from '@testing-library/react'
 import Greeting from "./Greeting"
 
-test('Greeting이라는 텍스트 렌더링을 합니다.', () => {
-    // Given
-    render(<Greeting />);
-    // When 렌더링
+describe('Greeting Component', () => {
+    test('Greeting이라는 텍스트 렌더링을 합니다.', () => {
+        // Given
+        render(<Greeting />);
+        // When 렌더링
 
-    // Then
-    const GreetingElement = screen.getByText(/Greeting/); // Greeting이라는 스트링 값을 가진 screen element, 없다면 null
-    expect(GreetingElement).toBeInTheDocument(); // GreetingElement가 Document 안에 있으면 true, 없으면 false
+        // Then
+        const GreetingElement = screen.getByText(/Greeting/); // Greeting이라는 스트링 값을 가진 screen element, 없다면 null
+        expect(GreetingElement).toBeInTheDocument(); // GreetingElement가 Document 안에 있으면 true, 없으면 false
+    });
+
+    test('만나서 반갑습니다 라는 텍스트 렌더링을 합니다.', () => {
+        // Given
+        render(<Greeting />);
+        // When 렌더링
+
+        // Then
+        const HelloElement = screen.getByText(/만나서 반갑습니다/);
+        expect(HelloElement).toBeInTheDocument();
+    });
+    
+    test('안녕하세요 라는 텍스트 렌더링을 합니다.', () => {
+        // Given
+        render(<Greeting />);
+        // When 렌더링
+
+        // Then
+        const HelloElement = screen.getByText(/안녕하세요/);
+        expect(HelloElement).toBeInTheDocument();
+    })
 })
+
