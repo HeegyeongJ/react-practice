3809f9040271d7fe5015eaf9ee58807342267793
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-02 15:06:06 +0900

    loading 및 error 처리

--- src/components/NewsList/index.js
+++ src/components/NewsList/index.js
@@ -3,6 +3,7 @@ import NewsItem from '../NewsItem';
 
 const NewsList = () => {
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     getNewsList();
@@ -12,16 +13,23 @@ const NewsList = () => {
     // await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-10-02&sortBy=publishedAt&apiKey=698fb8601daf4b75b37ddc98ccd12a8c')
     //   .then((response) => response.json())
     //   .then(data => setNews(data));
-
-    const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-10-02&sortBy=publishedAt&apiKey=698fb8601daf4b75b37ddc98ccd12a8c');
-    const data = await response.json();
-    setNews(data.articles)
+    try {
+      setIsLoading(true);
+      const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-10-02&sortBy=publishedAt&apiKey=698fb8601daf4b75b37ddc98ccd12a8c');
+      const data = await response.json();
+      setNews(data.articles)
+      setIsLoading(false)
+    } catch (error) {
+      console.error(error.message)
+    }
+    
   }
 
 
   return (
     <div>
-      {news.map(news => <NewsItem {...news} />)}
+      {!isLoading && news.map((news, index) => <NewsItem key={`news-${index}`} {...news} />)}
+      {isLoading && <p>로딩중입니다.</p>}
     </div>
   );
 };
