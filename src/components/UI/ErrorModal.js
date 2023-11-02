4b72d2e3cda764d9a4cc37e937f830e501f4c012
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-02 16:03:20 +0900

    debug: return

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -3,20 +3,22 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 const ModalOverlay = (props) => {
-  <Card className={classes.modal}>
-    <header className={classes.header}>
-      <h2>{props.title}</h2>
-    </header>
-    <div className={classes.content}>
-      <p>{props.message}</p>
-    </div>
-    <footer className={classes.actions}>
-      <Button onClick={props.onConfirm}>Okay</Button>
-    </footer>
-  </Card>
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  )
 }
 
 const Backdrop = (props) => {
