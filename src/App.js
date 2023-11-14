61e69f4996b08947aca1033712c802782844a0e5
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-15 00:46:53 +0900

    4-9 브랜치 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import Counter from './components/Counter';
-import Auth from './components/Auth';
-import Header from './components/Header';
+import React, { useEffect, useState } from 'react';
 
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // if loggined -> isLoggedIn = true
+    const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn')
+    if ( storedUserLoggedInInfo === 1) setIsLoggedIn(true);
+    // if not loggined -> isLoggedIn = false
+    else setIsLoggedIn(false) // 랜더링 다시 하면 초기화 되기 때문에 다시 넣지 않아도 됨
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
   return (
-    <>
-      {/*<Header />*/}
-      {/*<Auth />*/}
-      <Counter />
-    </>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
