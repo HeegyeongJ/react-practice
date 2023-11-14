cc1b71c697123dfdcbdf5b0ad42e449592dc0d03
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-14 23:37:01 +0900

    처음 렌더링시에만 로그인 여부 확인하는 useEffect 로직

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    // 만약 로그인이 되어 있다면 isLoggedIn = true
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInfo === "1") setIsLoggedIn(true);
+    // 만약 로그인이 되어 있지 않다면 isLoggedIn = false, 새로고침시 useState 초기화 돼서 따로 설정할 필요X
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
   };
 
   return (
