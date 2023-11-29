fa07db6a1bf5d64edb93546e1514dea7b5ae9b79
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-29 11:24:08 +0900

    filter 기능

--- src/components/Payments/Expenses.js
+++ src/components/Payments/Expenses.js
@@ -1,32 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-	const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-	const filterChangeHandler = (selectedYear) => {
-		setFilteredYear(selectedYear);
-	};
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-	return (
-		<Card className="expenses">
-			<ExpensesFilter
-				selected={filteredYear}
-				onChangeFilter={filterChangeHandler}
-			/>
-			{props.items.map((item) => (
-				<ExpenseItem
-					title={item.title}
-					amount={item.amount}
-					date={item.date}
-				/>
-			))}
-		</Card>
-	);
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {filteredExpenses.length > 0 ? (
+        filteredExpenses.map((item) => (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))
+      ) : (
+        <p>값이 없습니다.</p>
+      )}
+    </Card>
+  );
 };
 
 export default Expenses;
